492886cec901d1d06aca46c6b308ed315457d545
Atsuya Morita <amazingpower1315@gmail.com>
2023-11-02 11:12:05 +0900

    LIsence

--- test.bash
+++ test.bash
@@ -1,5 +1,16 @@
 #!/bin/bash
+#SPDX-FileCopyrighText: 2023 Atsuya Morita
 
+ng () {
+	echo NG at Lineb$1
+	res=1
+}
+
+res=0
+
+###I/O TEST##
 out=$(seq 5 | ./plus)
-
-["${out}=15"]
+[ "${out}" = 14 ] || ng ${LINENO}
+　
+[ "$res" = 0 ] && echo OK        # &&（AND記号）は左側が成功すると右側を実行
+exit $res
