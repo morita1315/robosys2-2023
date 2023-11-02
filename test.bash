5463c10f1f2ab4a22dbab8c9b419fcd85993d9ca
Atsuya Morita <amazingpower1315@gmail.com>
2023-11-02 11:23:32 +0900

    LIsence

--- test.bash
+++ test.bash
@@ -11,6 +11,15 @@ res=0
 ###I/O TEST##
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+ 	 out=$(echo あ | ./plus)
+ 	 [ "$?" = 1 ]      || ng ${LINENO}
+   	 [ "${out}" = "" ] || ng ${LINENO}
+          　 
+	 out=$(echo | ./plus) #空文字
+	 [ "$?" = 1 ]      || ng ${LINENO}
+	 [ "${out}" = "" ] || ng ${LINENO}
 　
 [ "$res" = 0 ] && echo OK        # &&（AND記号）は左側が成功すると右側を実行
 exit $res
